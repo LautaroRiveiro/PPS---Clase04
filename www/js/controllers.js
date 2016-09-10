7543b5147241729e295309ae3e5b7c648a7a2cf9
lautaroriveiro <lautaroriveiro91@gmail.com>
2016-09-10 12:01:04 -0300

    Instalar Plugins *Plugin de linterna *Plugin de sonidos

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -21,18 +21,18 @@ angular.module('starter.controllers', [])
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
-.controller('AccountCtrl', function($scope, $cordovaVibration) {
+.controller('AccountCtrl', function($scope, $cordovaVibration, $cordovaFlashlight, $cordovaNativeAudio) {
   $scope.settings = {
     enableFriends: true
   };
 
-
+  //Funciones del plugin Vibrar
   $scope.Vibrar = function(){
     try{
       $cordovaVibration.vibrate(10000); //Vibrar 10s
     }
     catch (err){
-      console.log(err);
+      console.log("Funciona la vibración en el celular, pero no se puede en la PC");
     }
   }
 
@@ -41,9 +41,33 @@ angular.module('starter.controllers', [])
       $cordovaVibration.vibrate(0);
     }
     catch (err){
-      console.log(err);
+      console.log("Funciona detener la vibración en el celular, pero no se puede en la PC");
     }
   }
 
+  //Funciones del plugin Linterna
+  $scope.Linterna = function(){
+    $cordovaFlashlight.switchOn();
+  }
 
+  $scope.DetenerLinterna = function(){
+    $cordovaFlashlight.switchOff()
+  }
+
+if( window.plugins && window.plugins.NativeAudio ){
+  //Funciones del plugin Sonidos
+  $cordovaNativeAudio
+    .preloadSimple('click', 'audio/1.mp3')
+    .then(function (msg) {
+      console.log(msg);
+    }, function (error) {
+      alert(error);
+    });
+
+  $scope.Sonido = function(){
+    $cordovaNativeAudio.play('click');
+    //$cordovaNativeAudio.loop('music');
+  }
+
+}
 });
